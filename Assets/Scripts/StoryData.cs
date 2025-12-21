4f7a54d61411da476cd2a49e9631e007107be5b7
beyzabaslik <bslkbeyza@gmail.com>
2025-12-21 23:04:23 +0300

    Level 5 Scene Design Finished
    Added level 5 full design with own scene

--- Assets/Scripts/StoryData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "StoryData", menuName = "Echoes/Story Data")]
-public class StoryData : ScriptableObject
-{
-    [TextArea(4, 12)]
-    public string[] pages;
-}
-
